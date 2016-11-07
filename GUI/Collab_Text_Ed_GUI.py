539f768f895db3621c4987f0ad2ce1b77dc96a52
additives <riho11@msn.com>
2016-11-07 20:49:09 +0200

    GUI added
    Added very basic GUI, need more functionality

---
+++ GUI/Collab_Text_Ed_GUI.py
@@ -0,0 +1,123 @@
+import Tkinter as tk
+from tkSimpleDialog import askstring
+from tkFileDialog   import asksaveasfilename
+
+from tkMessageBox import askokcancel
+ 
+
+class TextEdGUI(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        tk.Tk.title(self, "Collab Text Editor")
+        tk.Tk.geometry(self, '580x410')
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+# **** Menubar stuff *****
+        menubar = tk.Menu(container)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Open file",
+                             command=lambda:popupmsg("Not supported yet!"))
+        filemenu.add_command(label="Exit", command=quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+
+        editmenu = tk.Menu(menubar, tearoff=0)
+        editmenu.add_command(label="Copy",
+                             command=lambda:popupmsg("Sorry no can do, sir!"))
+        menubar.add_cascade(label="Edit", menu=editmenu)
+
+        tk.Tk.config(self, menu=menubar)
+
+# **** Status bar stuff ****
+
+        status = tk.Label(container, text="Connected clients:", bd=1,
+                          relief=tk.SUNKEN, anchor=tk.W)
+        status.grid(row=3, column=0, sticky="sw")
+
+        self.frames = {}
+
+        for F in (ConnectPage, EditorPage):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(ConnectPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class ConnectPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        label = tk.Label(self, text="Connect Page", font=("Verdana", 12))
+        label.grid(pady = 10, padx = 10,row=0, column=0, sticky="new")
+
+        button1 = tk.Button(self, text="Connect",
+                            command=lambda: controller.show_frame(EditorPage))
+        button1.grid(row=2, column=3, padx=15)
+
+        label4 = tk.Label(self, text="Server IP")
+        label5 = tk.Label(self, text="Server Port")
+        label5.grid(row=1,column=0)
+        label4.grid(row=2, column=0)
+
+
+        entryText = tk.StringVar()
+        entry = tk.Entry(self, textvariable=entryText)
+        entryText.set("Default server Port")
+        entry.grid(row=1, column=1)
+
+        entryText2 = tk.StringVar()
+        entry2 = tk.Entry(self, textvariable=entryText2)
+        entryText2.set("Default server IP")
+        entry2.grid(row=2, column=1)
+
+
+
+
+class EditorPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        text = tk.Text(self , height=25, width=80)
+        text.grid(column=0, row=0, sticky="nw")
+        scroll = tk.Scrollbar(self, command=text.yview)
+        text.configure(yscrollcommand=scroll.set)
+        scroll.grid(column=1, row=0, sticky="ne", ipady=163)
+
+        label = tk.Label(self, text="Editor Page", font=("Verdana", 12))
+        label.grid(column=0, row=1, sticky="se")
+
+        label2 = tk.Label(self, text="Editor Page", font=("Verdana", 12))
+        label2.grid(column=0, row=1, sticky="s")
+
+        button2 = tk.Button(self, text="Disconnect",
+                            command=lambda: controller.show_frame(ConnectPage))
+        button2.grid(column=0,row=1, sticky="sw")
+
+
+        #text_widget.index(Tkinter.INSERT)
+
+def popupmsg(argument):
+    popup = tk.Tk()
+    popup.title("Pop up")
+    label=tk.Label(popup, text = argument)
+    label.pack(side="top", fill="x", pady=10)
+    B1 = tk.Button(popup, text="Ok", command=popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
+app = TextEdGUI()
+app.mainloop()
