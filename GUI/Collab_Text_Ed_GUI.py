928775b456b546b972d899b559e082be3df02d80
additives <riho11@msn.com>
2016-11-09 13:23:54 +0200

    GUI queue + threads added
    GUI can now get information from other threads through a queue

--- GUI/Collab_Text_Ed_GUI.py
+++ GUI/Collab_Text_Ed_GUI.py
@@ -1,4 +1,10 @@
 import Tkinter as tk
+import sys
+import Queue
+import threading
+import time
+import random
+
 from tkSimpleDialog import askstring
 from tkFileDialog   import asksaveasfilename
 
@@ -7,7 +13,7 @@ from tkMessageBox import askokcancel
 
 class TextEdGUI(tk.Tk):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, queue, endCommand, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         tk.Tk.title(self, "Collab Text Editor")
         tk.Tk.geometry(self, '580x410')
@@ -17,28 +23,15 @@ class TextEdGUI(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-# **** Menubar stuff *****
-        menubar = tk.Menu(container)
-        filemenu = tk.Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Open file",
-                             command=lambda:popupmsg("Not supported yet!"))
-        filemenu.add_command(label="Exit", command=quit)
-        menubar.add_cascade(label="File", menu=filemenu)
+        self.menubar(container, endCommand)
+        self.statusbar(container)
 
-        editmenu = tk.Menu(menubar, tearoff=0)
-        editmenu.add_command(label="Copy",
-                             command=lambda:popupmsg("Sorry no can do, sir!"))
-        menubar.add_cascade(label="Edit", menu=editmenu)
-
-        tk.Tk.config(self, menu=menubar)
-
-# **** Status bar stuff ****
-
-        status = tk.Label(container, text="Connected clients:", bd=1,
-                          relief=tk.SUNKEN, anchor=tk.W)
-        status.grid(row=3, column=0, sticky="sw")
+        self.protocol("WM_DELETE_WINDOW", endCommand) # Handles when GUI is closed from X
+        self.bind('<Escape>',lambda x: endCommand()) # Make Esc also exit the program
 
         self.frames = {}
+
+        self.queue = queue
 
         for F in (ConnectPage, EditorPage):
             frame = F(container, self)
@@ -52,6 +45,29 @@ class TextEdGUI(tk.Tk):
         self.show_frame(ConnectPage)
 
 
+    def menubar(self, container, endCommand):  # **** Menubar stuff *****
+        menubar = tk.Menu(container)
+
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Open file",
+                                 command=lambda: popupmsg("Not supported yet!"))
+        filemenu.add_command(label="Exit", command=endCommand)
+        menubar.add_cascade(label="File", menu=filemenu)
+
+        editmenu = tk.Menu(menubar, tearoff=0)
+        editmenu.add_command(label="Copy",
+                                 command=lambda: popupmsg("Sorry no can do, sir!"))
+        menubar.add_cascade(label="Edit", menu=editmenu)
+
+        tk.Tk.config(self, menu=menubar)
+
+
+    def statusbar(self, container): # **** Status bar stuff ****
+
+        status = tk.Label(container, text="Connected clients:", bd=1,
+                          relief=tk.SUNKEN, anchor=tk.W)
+        status.grid(row=3, column=0, sticky="sw")
+
 
     def show_frame(self, cont):
 
@@ -60,6 +76,16 @@ class TextEdGUI(tk.Tk):
 
     def get_page(self, page_name):
         return self.frames[page_name]
+
+    def processIncoming(self):
+        """Handle all messages currently in the queue, if any."""
+        while self.queue.qsize():
+            try:
+                msg = self.queue.get(0)
+                self.get_page("EditorPage").text.insert("1.0", str(msg)+'\n')
+                #print msg
+            except Queue.Empty:
+                pass
 
 
 class ConnectPage(tk.Frame):
@@ -98,7 +124,6 @@ class EditorPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        #text = tk.Text(self , height=25, width=80)
         self.text = tk.Text(self, height=25, width=80)
 
         self.text.grid(column=0, row=0, sticky="nw")
@@ -127,7 +152,38 @@ def popupmsg(argument):
     popup.mainloop()
 
 
-app = TextEdGUI()
-app.get_page("EditorPage").text.insert("1.0", "Hello, world!")
-app.mainloop()
+class ThreadedClient(threading.Thread):
+
+    def __init__(self):
+
+        self.queue = Queue.Queue()
+
+        self.gui = TextEdGUI(self.queue, self.endApplication)
+        self.gui.get_page("EditorPage").text.insert("1.0","Hello, world!")
+
+        self.running = 1
+        self.thread1 = threading.Thread(target=self.workerThread1)
+        self.thread1.start()
+
+        self.periodicCall() # Periodic call to check if the queue contains something
+
+    def periodicCall(self):
+        self.gui.processIncoming()
+        if not self.running:
+            sys.exit(1)
+        self.gui.after(200, self.periodicCall) # Check every 200 ms if there is something new in the queue.
+
+    def workerThread1(self):
+
+        while self.running:
+            time.sleep(random.random() * 1.5)
+            msg = random.random()
+            self.queue.put(msg)
+
+    def endApplication(self):
+        self.running = 0
+
+if __name__ == '__main__':
+    client = ThreadedClient()
+    client.gui.mainloop()
 
