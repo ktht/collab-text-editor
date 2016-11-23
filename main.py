f45033a8a87347b0d3a302cbf8c140bd372798cb
additives <riho11@msn.com>
2016-11-23 20:56:34 +0200

    main bugfixes + new file functionality

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ logging.basicConfig(
   )
 
 queue_send2srvr = Queue.Queue()
+useListElem = True
 
 class TextEdGUI(tk.Tk):
 
@@ -81,10 +82,17 @@ class TextEdGUI(tk.Tk):
         self.get_page("EditorPage").updateQueue()
         while client.queue_incoming.qsize():
             try:
-                msg = client.queue_incoming.get(0)
-                #print(self.get_page("ConnectPage").entryText.get())
-                self.get_page("EditorPage").text.insert("1.0", str(msg)+'\n')
-                #print msg
+                try:
+                    msg = client.queue_incoming.get(0)
+                    print("Parast handshake: " + str(msg))
+                    line_no, action, payload = backend.common.unmarshall(msg)
+                    #print(self.get_page("ConnectPage").entryText.get())
+                    self.get_page("EditorPage").text.insert(str(line_no+1)+".0", str(payload))
+                    #print msg
+                except Exception:
+                    #msg = client.queue_incoming.get(0)
+                    print("Enne handshake: " + str(msg))
+                    self.get_page("EditorPage").text.insert("1.0", str(msg) + '\n')
             except Queue.Empty:
                 pass
 
@@ -170,7 +178,7 @@ class SelectorPage(tk.Frame):
         label8 = tk.Label(self, text="File").grid(row=2, column=0, pady=5)
         label8 = tk.Label(self, text="Files").grid(row=3, column=0, pady=5)
 
-        tk.Button(self, text='Select', command=lambda: controller.show_frame(EditorPage)
+        tk.Button(self, text='Select', command=lambda: self.funcs3(self.controller)
                   ).grid(row=2, column=2, padx=15, pady=5)
 
         #button3 = tk.Button(self, text="Disconnect",
@@ -183,17 +191,17 @@ class SelectorPage(tk.Frame):
 
         self.entryText4 = tk.StringVar()
         entry4 = tk.Entry(self, textvariable=self.entryText4).grid(row=1, column=1)
-        self.entryText4.set("Files user ID")
+        #self.entryText4.set("Files user ID")
 
         self.entryText5 = tk.StringVar()
         entry4 = tk.Entry(self, textvariable=self.entryText5).grid(row=2, column=1)
-        self.entryText5.set("File ID")
+        #self.entryText5.set("File ID")
 
         self.listBox = tk.Listbox(self)
         self.listBox.grid(row=3, column=1, pady=10, padx=10)
 
         self.var = tk.IntVar()
-        checkBox = tk.Checkbutton(self, text="Public", variable=self.var, command=self.checkingBox)
+        checkBox = tk.Checkbutton(self, text="Private", variable=self.var, command=self.checkingBox)
         checkBox.grid(row = 1, column = 2)
 
     def checkingBox(self):
@@ -209,9 +217,17 @@ class SelectorPage(tk.Frame):
 
     def funcs2(self, contr):
         contr.show_frame(EditorPage)
-        self.select_Listelem
+        #self.select_Listelem
         client1.e.set()
         client1.e.clear()
+
+    def funcs3(self, contr):
+        contr.show_frame(EditorPage)
+        global useListElem
+        useListElem = False
+        client1.e.set()
+        client1.e.clear()
+
 
 
 class EditorPage(tk.Frame):
@@ -229,12 +245,6 @@ class EditorPage(tk.Frame):
         scroll = tk.Scrollbar(self)
         self.text.configure(yscrollcommand=scroll.set)
         scroll.grid(column=1, row=0, sticky="ne", ipady=163)
-
-        #label = tk.Label(self, text="Editor Page", font=("Verdana", 12))
-        #label.grid(column=0, row=1, sticky="se")
-
-        #label2 = tk.Label(self, text="Editor Page", font=("Verdana", 12))
-        #label2.grid(column=0, row=1, sticky="s")
 
         button5 = tk.Button(self, text="Back",
                             command=lambda: controller.show_frame(SelectorPage))
@@ -259,7 +269,7 @@ class EditorPage(tk.Frame):
             while len(self.text_muudatused) > 0:
                 try:
                     rida = self.text_muudatused.pop()
-                    queue_send2srvr.put(str(str(rida) + ":" + self.text.get(str(rida) + ".0", str(
+                    queue_send2srvr.put(str(str(rida-1) + ":" + self.text.get(str(rida) + ".0", str(
                         rida + 1) + ".0").rstrip()))
                 except UnicodeEncodeError:
                     print("Sisestatud char ei sobi (pole ascii koodis olemas)!")
@@ -301,21 +311,15 @@ class ThreadedClient(threading.Thread):
         self.test_text = 'This is the long text'
 
         self.gui = TextEdGUI( self.endApplication)
-        #self.gui.get_page("EditorPage").text.insert("1.0","Hello, world!")
 
         self.running = 1
         self.e = threading.Event()
-        self.thread2 = threading.Thread(target=self.send2srvr_Thread, name="clientSenderThread")
         self.thread3 = threading.Thread(target = self.clientThread, name="clientThread")
         self.thread3.setDaemon(True)
-        #self.thread2.setDaemon(True)
         self.create_new_ID = False
-        self.thread2.start()
         self.thread3.start()
 
         self.periodicCall() # Periodic call to check if the queue contains something
-
-
 
         if not os.path.exists(backend.common.TMP_DIR_CLIENT):
             os.makedirs(backend.common.TMP_DIR_CLIENT)
@@ -347,24 +351,11 @@ class ThreadedClient(threading.Thread):
         self.gui.after(200, self.periodicCall) # Check every 200 ms if there is something new in the queue.
 
 
-    def send2srvr_Thread(self):
-        """Handle all messages currently in the queue, if any."""
-        while self.running:
-            time.sleep(0.2)
-            while queue_send2srvr.qsize():
-                try:
-                    msg = queue_send2srvr.get(0)
-                    print msg
-                except Queue.Empty:
-                    pass
-
-
     def clientThread(self):
         self.e.wait()
         usr_ID = self.gui.get_page("ConnectPage").entryText.get()
         server_IP  = self.gui.get_page("ConnectPage").entryText2.get()
         server_PORT = int(self.gui.get_page("ConnectPage").entryText3.get())
-        #print("User ID:"+str(usr_ID))
 
         if not usr_ID.isdigit():
             usr_ID = 1
@@ -391,22 +382,44 @@ class ThreadedClient(threading.Thread):
 
 
             self.e.wait()
-            fname = self.gui.get_page("SelectorPage").listBox.get(self.gui.get_page("SelectorPage").listBox.curselection())
-            file_contents = c.req_file(backend.common.DELIM_ID_FILE.join([str(client_id), fname.split(':')[1]]))
 
-            client.queue_incoming.put(file_contents)
+            if useListElem:
+                try:
+                    print("Using list!")
+                    fname = self.gui.get_page("SelectorPage").listBox.get(
+                        self.gui.get_page("SelectorPage").listBox.curselection())
+                    file_contents = c.req_file(backend.common.DELIM_ID_FILE.join([str(client_id), fname.split(':')[1]]))
+                    client.queue_incoming.put(file_contents)
+                except Exception:
+                    print('Nothing has been selected from the list!')
+            else:
+                fname = self.gui.get_page("SelectorPage").entryText4.get()
+                cl_ID = self.gui.get_page("SelectorPage").entryText5.get()
+                file_contents = c.req_file(backend.common.DELIM_ID_FILE.join([str(fname), str(cl_ID)]))
+                #client.queue_incoming.put(file_contents)
 
-
-
-            #ret_changes = c.send_changes(0, backend.common.EDIT_REPLACE, self.test_text)
-            #if ret_changes:
-            #    print("WOOO, SUCCESS!")
-            #else:
-            #    print("ERROR!")
 
             while(self.running):
+                while queue_send2srvr.qsize():
+                    try:
+                        msg = queue_send2srvr.get(0)
+                        msg = msg.split(':')
+                        c.send_changes(int(msg[0]), backend.common.EDIT_INSERT, str(msg[1]))
+                        print msg
+                    except Queue.Empty:
+                        pass
                 time.sleep(0.2)
 
+    #def send_changes(self, line_no, action, payload=''):
+        '''Send changes in local file to the server
+        :param line_no: int, Line number to be changed (line numbers start from 0)
+        :param action:  int, Editing control code
+                             (valid values: common.EDIT_REPLACE, common.EDIT_INSERT, common.EDIT_DELETE)
+        :param payload: string, Value for the line (ignored if `action' is set to common.EDIT_DELETE)
+        :return: True, if the request was successfully sent
+                 False, otherwise
+        The file and client ID relevant to this request are defined in instance variables `self.id' and `self.fn'
+        '''
 
     def endApplication(self):
         self.running = 0
